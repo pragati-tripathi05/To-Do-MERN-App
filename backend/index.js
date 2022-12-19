4b253d4cf12fc9091cb59770ea93be13d676877c
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-12-19 18:00:33 +0530

    initializing

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,10 @@ const cors = require("cors");
 
 const app = express();
 
+const PORT = process.env.PORT || 3200;
+
 app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
