646b1badd492c1327028da9cf32abeca36171c43
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-12-20 17:58:33 +0530

    todo tasks strips

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Welcome to your MERN Todo-app</h1>
+      <h4>Your Tasks</h4>
+
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">New task1</div>
+          <div className="delete-todo">X</div>
+        </div>
+        <div className="todo is-complete">
+          <div className="checkbox"></div>
+          <div className="text">New task2</div>
+          <div className="delete-todo">X</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
