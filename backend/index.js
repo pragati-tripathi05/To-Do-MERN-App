08775d1ce56212e2ce323d6a6422c2857dcbd418
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-12-20 10:36:18 +0530

    initializing

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const connection = require("./config/db");
 require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3200;
+const PORT = process.env.PORT || 4500;
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to db successfully");
+  } catch (err) {
+    console.log("Error in db connection");
+    console.log(err);
+  }
   console.log(`Listening on ${PORT}`);
 });
