47b8a5c5aa21c2a9e797168c5094d39e312b243a
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-12-20 17:25:47 +0530

    todo api CRUD done

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,31 @@ const PORT = process.env.PORT || 4500;
 app.use(express.json());
 app.use(cors());
 
+// GET or READ
 app.get("/todos", async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
 
+// POST or CREATE
 app.post("/todo/new", (req, res) => {
   const todo = new Todo({
     text: req.body.text,
   });
+  todo.save();
+  res.json(todo);
+});
+
+// DELETE
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json(result);
+});
+
+// TOGGLE complete & pending
+app.put("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.complete = !todo.complete;
   todo.save();
   res.json(todo);
 });
