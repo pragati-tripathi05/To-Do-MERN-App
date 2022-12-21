a90cc396c6383ce9a76ad7aad9caa7f84ee73959
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-12-21 07:04:34 +0530

    new tasks popup added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,6 +43,21 @@ function App() {
     setTodos((todos) => todos.filter((todo) => todo._id !== data._id));
   };
 
+  // ADD-TODO popup
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: newTodo }),
+    }).then((res) => res.json());
+
+    setTodos([...todos, data]);
+    setPopupActive(false);
+    setNewTodo("");
+  };
+
   return (
     <div className="App">
       <h1>Welcome to your MERN Todo-app</h1>
@@ -65,10 +80,35 @@ function App() {
             </div>
           );
         })}
+
+        {/* New Tasks */}
       </div>
       <div className="addPopup" onClick={() => setPopupActive(true)}>
         +
       </div>
+
+      {/* Popup for new Tasks */}
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            X
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="buttons" onClick={addTodo}>
+              CREATE TASK
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
